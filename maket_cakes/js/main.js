811407b06fbf462d19269368ae33f6dff4f242b8
Olesya Takhtay <52999811+Olesya18@users.noreply.github.com>
2025-11-11 13:53:31 +0700

    Update main.js

--- maket_cakes/js/main.js
+++ maket_cakes/js/main.js
@@ -160,3 +160,11 @@ new Swiper(".gallery__slider", {
             },
         },
     });
+
+    //маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+
+    const im = new Inputmask('+7(999) 999-99-99')
+
+    im.mask(telInputs)
