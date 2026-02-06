ee0617b85b773286a831fe9f39619621eef09978
Olesya Takhtay <52999811+Olesya18@users.noreply.github.com>
2026-02-06 15:35:44 +0700

    Add files via upload

---
+++ урок 1_js/main.js
@@ -0,0 +1,24 @@
+//Задача 1
+const firstName = "Olesya";
+const lastName = "Takhtay";
+const isStudent = true;
+
+//Задача 2
+const age = 26;
+const currentYear = 2026;
+const birthYear = currentYear - age;
+
+//Задача 3
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик/ученица курса: ${isStudent}.`);
+
+// Задача 4.
+// Какое значение будет у переменной result?
+//let a = '123';
+//let b = +'456';
+//let c = Number('789');
+//let d = Boolean(0);
+//let e = Boolean(' ');
+//let result = a + b + c + d + e;
+
+
+//result = 123456789falsetrue
